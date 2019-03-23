1c65392b3587954e873d80ad0a6a7483c916883b
Mark Romero <markromero@Marks-MacBook-Pro.local>
2019-03-23 11:31:46 -0600

    completed stage 2

--- stage-2/src/Components/StoreFront/StoreFront.js
+++ stage-2/src/Components/StoreFront/StoreFront.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './StoreFront.css';
 
 class StoreFront extends Component {
+
   render() {
     let productDisplay = this.props.products.map((element, index) => {
       return (
@@ -16,6 +17,7 @@ class StoreFront extends Component {
     })
     return (
       <div className="storefront-container">
+        {productDisplay}
       </div>
     )
   }
